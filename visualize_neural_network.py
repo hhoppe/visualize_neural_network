46cdc77b87473ce1fd0de33a009d6a7007784001
Hugues Hoppe <hhoppe@gmail.com>
2024-01-11 22:57:12 -0800

    Introduce class MainPlot

--- visualize_neural_network.py
+++ visualize_neural_network.py
@@ -13,7 +13,6 @@
 
 import contextlib
 import math
-from typing import Any
 
 import IPython.display
 import ipywidgets
@@ -28,6 +27,7 @@ VERBOSE = False
 R = 10
 
 # %%
+# Numbers of nodes in each layer of the neural network (layer 0 is input, last layer is output):
 # LAYER_SIZES = 1, 3, 1
 LAYER_SIZES = 1, 2, 2, 1
 # LAYER_SIZES = 1, 3, 3, 1
@@ -69,9 +69,7 @@ def sigmoid(x):
   return torch.sigmoid(x) if isinstance(x, torch.Tensor) else 1 / (1 + np.exp(-x))
 
 
-# %%
 ACTIVATIONS = {'relu': relu, 'leaky_relu': leaky_relu, 'identity': identity, 'sigmoid': sigmoid}
-
 
 # %% [markdown]
 # ## Neural network
@@ -80,22 +78,21 @@ ACTIVATIONS = {'relu': relu, 'leaky_relu': leaky_relu, 'identity': identity, 'si
 # %%
 # The dictionary `params` contains weights 'w{layer}{curr_index}{prev_index}' and
 # biases 'b{layer}{index}', as either float values or (scalar) torch tensors.
-def neural_network(x, params, output_layer_index=10**9):
-  assert len(x) == LAYER_SIZES[0]
-  prev_nodes = x
+def neural_network(inputs, params, activations, output_layer_index=10**9):
+  assert len(inputs) == LAYER_SIZES[0]
+  outputs = inputs
   for layer_index, layer_size in enumerate(LAYER_SIZES[1 : output_layer_index + 1], 1):
-    is_last_layer = layer_index == len(LAYER_SIZES) - 1
-    activation: Any = ACTIVATIONS[activation_selector.value] if not is_last_layer else identity
+    activation = activations[layer_index]
     nodes = []
     for node_index in range(layer_size):
       linear = sum(
           params[f'w{layer_index}{node_index}{prev_index}'] * prev_node
-          for prev_index, prev_node in enumerate(prev_nodes)
+          for prev_index, prev_node in enumerate(outputs)
       )
-      node = activation(linear + params[f'b{layer_index}{node_index}'])
+      node = activation(linear + params[f'b{layer_index}{node_index}'])  # type: ignore[operator]
       nodes.append(node)
-    prev_nodes = nodes
-  return prev_nodes
+    outputs = nodes
+  return outputs
 
 
 # %% [markdown]
@@ -103,7 +100,7 @@ def neural_network(x, params, output_layer_index=10**9):
 
 
 # %%
-def get_reset_values():
+def get_reset_params():
   d: dict[str, float] = {}
 
   if LAYER_SIZES == (1, 3, 1):
@@ -126,36 +123,32 @@ def get_reset_values():
   return d
 
 
-RESET_VALUES = get_reset_values()
-params = RESET_VALUES
-
-
 # %% [markdown]
 # ## Neural network optimization
 
 
 # %%
-def fit_neural_network(num_steps=1500):
+def fit_neural_network(params, activations, target_function, num_steps=1500):
   tensor_params = {
       key: torch.tensor([value], dtype=torch.float32, requires_grad=True)
       for key, value in params.items()
   }
   variables = list(tensor_params.values())
   x_train = torch.linspace(0, 1, 100, dtype=torch.float32)
-  y_train = TARGETS[target_selector.value](x_train)
+  y_train = target_function(x_train)
 
   learning_rate = 0.01
   optimizer = torch.optim.Adam(variables, lr=learning_rate)
   for step in range(num_steps):
     optimizer.zero_grad()
-    (y_pred,) = neural_network([x_train], tensor_params)
+    (y_pred,) = neural_network([x_train], tensor_params, activations)
     loss = torch.mean((y_train - y_pred) ** 2)
     loss.backward()
     optimizer.step()
     if VERBOSE and step % 100 == 0:
       print(f'Step {step}, Loss: {loss.item()}')  # See messages using "log" icon.
 
-  update_params({key: tensor.detach().numpy().item() for key, tensor in tensor_params.items()})
+  return {key: tensor.detach().numpy().item() for key, tensor in tensor_params.items()}
 
 
 # %% [markdown]
@@ -183,118 +176,129 @@ def display_network_graph():
 
 
 # %%
-def create_sliders():
-  a = dict(step=0.01, continuous_update=True)
-  return {
-      key: ipywidgets.FloatSlider(value=value, min=value - R, max=value + R, description=key, **a)
-      for key, value in params.items()
-  }
+class MainPlot:
+  """User interface for main plot."""
 
+  def __init__(self):
+    self.params = get_reset_params()
+    self.target_selector = ipywidgets.RadioButtons(
+        options=list(TARGETS), value='sine', description='Target function:'
+    )
+    self.activation_selector = ipywidgets.RadioButtons(
+        options=list(ACTIVATIONS), value='leaky_relu', description='Activation function:'
+    )
+    self.layer_selector = ipywidgets.RadioButtons(
+        options=range(len(LAYER_SIZES)), value=len(LAYER_SIZES[1:]), description='Plot layer:'
+    )
+    self.sliders = self.create_sliders() if SHOW_SLIDERS else {}
+    self.center_button = ipywidgets.Button(description='Center slider ranges')
+    self.reset_button = ipywidgets.Button(description='Reset params')
+    self.randomize_button = ipywidgets.Button(description='Randomize params')
+    self.fit_button = ipywidgets.Button(description='Fit target')
+    self.output = ipywidgets.Output()
+    self.textarea = ipywidgets.Textarea(
+        rows=len(LAYER_SIZES[1:]), layout={'width': '800px'}, disabled=True
+    )
 
-# %%
-target_selector = ipywidgets.RadioButtons(
-    options=list(TARGETS), value='sine', description='Target function:'
-)
-activation_selector = ipywidgets.RadioButtons(
-    options=list(ACTIVATIONS), value='leaky_relu', description='Activation function:'
-)
-layer_selector = ipywidgets.RadioButtons(
-    options=range(len(LAYER_SIZES)), value=len(LAYER_SIZES[1:]), description='Plot layer:'
-)
-sliders = create_sliders() if SHOW_SLIDERS else {}
-center_button = ipywidgets.Button(description='Center slider ranges')
-reset_button = ipywidgets.Button(description='Reset params')
-randomize_button = ipywidgets.Button(description='Randomize params')
-fit_button = ipywidgets.Button(description='Fit target')
-output = ipywidgets.Output()
-textarea = ipywidgets.Textarea(rows=len(LAYER_SIZES[1:]), layout={'width': '800px'}, disabled=True)
+    self.target_selector.observe(lambda change: self.update_plot(), names='value')
+    self.activation_selector.observe(lambda change: self.update_plot(), names='value')
+    self.layer_selector.observe(lambda change: self.update_plot(), names='value')
+    for slider in self.sliders.values():
+      slider.observe(lambda change: self.on_slider_change(), 'value')
+    self.center_button.on_click(lambda button: self.center_slider_range())
+    # (Due to a bug in slider value update, we may need to press a button more than once.)
+    self.reset_button.on_click(lambda button: self.reset_parameters())
+    self.randomize_button.on_click(lambda button: self.randomize_parameters())
+    self.fit_button.on_click(lambda button: self.fit_target())
 
+  def create_sliders(self):
+    a = dict(step=0.01, continuous_update=True)
+    return {
+        key: ipywidgets.FloatSlider(value=value, min=value - R, max=value + R, description=key, **a)
+        for key, value in self.params.items()
+    }
 
-# %%
-def update_plot():
-  text_lines = []
-  for layer_index in range(1, len(LAYER_SIZES)):
-    layer_str = ', '.join(f'{k}={v:.2f}' for k, v in params.items() if int(k[1]) == layer_index)
-    text_lines.append(f'dict({layer_str})')
-  textarea.value = '\n'.join(text_lines)
-  with output:
-    output.clear_output(wait=True)
-    _, ax = plt.subplots(figsize=(10, 5))
-    x = np.linspace(0, 1, 100)
-    ax.plot(x, TARGETS[target_selector.value](x), lw=0.5, label='target')
-    output_layer_index = layer_selector.value
-    output_nodes = neural_network([x], params, output_layer_index=output_layer_index)
-    for node_index, node in enumerate(output_nodes):
-      ax.plot(x, node, label=f'node n{output_layer_index}{node_index}')
-    ax.set(xlabel='x', ylabel='y=f(x)', ylim=(-2, 2))
-    ax.grid(True)
-    ax.legend(loc='upper right')
-    plt.show()
+  def get_activations(self):
+    return [ACTIVATIONS[self.activation_selector.value]] * (len(LAYER_SIZES) - 1) + [identity]
 
+  def update_plot(self):
+    text_lines = []
+    for layer_index in range(1, len(LAYER_SIZES)):
+      t = ', '.join(f'{k}={v:.2f}' for k, v in self.params.items() if int(k[1]) == layer_index)
+      text_lines.append(f'dict({t})')
+    self.textarea.value = '\n'.join(text_lines)
+    with self.output:
+      self.output.clear_output(wait=True)
+      _, ax = plt.subplots(figsize=(10, 5))
+      x = np.linspace(0, 1, 100)
+      ax.plot(x, TARGETS[self.target_selector.value](x), lw=0.5, label='target')
+      output_layer_index = self.layer_selector.value
+      output_nodes = neural_network(
+          [x], self.params, self.get_activations(), output_layer_index=output_layer_index
+      )
+      for node_index, node in enumerate(output_nodes):
+        ax.plot(x, node, label=f'node n{output_layer_index}{node_index}')
+      ax.set(xlabel='x', ylabel='y=f(x)', ylim=(-2, 2))
+      ax.grid(True)
+      ax.legend(loc='upper right')
+      plt.show()
 
-# %%
-def update_params(new_params, center_sliders=True):
-  global params
-  params = new_params
-  if SHOW_SLIDERS:
-    with contextlib.ExitStack() as stack:
-      for slider in reversed(sliders.values()):
-        stack.enter_context(slider.hold_trait_notifications())
-      for key, slider in sliders.items():
-        value = params[key]
-        slider.value = value  # Bug: https://github.com/jupyter-widgets/ipywidgets/issues/3824.
-        if center_sliders:
-          slider.min = value - R
-          slider.max = value + R
-  update_plot()
+  def update_params(self, new_params, center_sliders=True):
+    self.params = new_params
+    if SHOW_SLIDERS:
+      with contextlib.ExitStack() as stack:
+        for slider in reversed(self.sliders.values()):
+          stack.enter_context(slider.hold_trait_notifications())
+        for key, slider in self.sliders.items():
+          value = self.params[key]
+          if slider.value != value:  # Not sure if test is useful.
+            slider.value = value  # Bug: https://github.com/jupyter-widgets/ipywidgets/issues/3824.
+          if center_sliders:
+            slider.min = value - R
+            slider.max = value + R
+    self.update_plot()
 
+  def on_slider_change(self):
+    self.update_params(
+        {key: slider.value for key, slider in self.sliders.items()}, center_sliders=False
+    )
 
-def on_slider_change():
-  update_params({key: slider.value for key, slider in sliders.items()}, center_sliders=False)
+  def center_slider_range(self):
+    for slider in self.sliders.values():
+      slider.min, slider.max = slider.value - R, slider.value + R
 
+  def reset_parameters(self):
+    self.update_params(get_reset_params())
 
-def center_slider_range():
-  for slider in sliders.values():
-    value = slider.value
-    slider.min, slider.max = value - R, value + R
+  def randomize_parameters(self):
+    rng = np.random.default_rng()  # Non-deterministic.
+    self.update_params({key: rng.random() * 2 - 1 for key in self.params})
 
+  def fit_target(self):
+    self.update_params(
+        fit_neural_network(self.params, self.get_activations(), TARGETS[self.target_selector.value])
+    )
 
-def reset_parameters():
-  update_params(RESET_VALUES)
-
-
-def randomize_parameters():
-  rng = np.random.default_rng()  # Non-deterministic.
-  update_params({key: rng.random() * 2 - 1 for key in params})
-
-
-# %%
-def display_main_ui():
-  target_selector.observe(lambda change: update_plot(), names='value')
-  activation_selector.observe(lambda change: update_plot(), names='value')
-  layer_selector.observe(lambda change: update_plot(), names='value')
-  for slider in sliders.values():
-    slider.observe(lambda change: on_slider_change(), 'value')
-  center_button.on_click(lambda button: center_slider_range())
-  # Due to a bug in slider value update, we may need to press a button more than once.
-  reset_button.on_click(lambda button: reset_parameters())
-  randomize_button.on_click(lambda button: randomize_parameters())
-  fit_button.on_click(lambda button: fit_neural_network())
-
-  reset_parameters()
-  selectors = ipywidgets.HBox([target_selector, activation_selector, layer_selector])
-  layers = [ipywidgets.Label('Weight and bias parameters in each network layer:')]
-  for layer_index in range(1, len(LAYER_SIZES)):
-    layer_sliders = [slider for key, slider in sliders.items() if int(key[1]) == layer_index]
-    layers.append(ipywidgets.HBox([ipywidgets.Label(f'Layer {layer_index}:')] + layer_sliders))
-  buttons = ipywidgets.HBox([center_button, reset_button, randomize_button, fit_button])
-  rows = [selectors, *layers, buttons, output, textarea]
-  IPython.display.display(ipywidgets.VBox(rows))
+  def display(self):
+    self.reset_parameters()
+    selectors = ipywidgets.HBox(
+        [self.target_selector, self.activation_selector, self.layer_selector]
+    )
+    layers = [ipywidgets.Label('Weight and bias parameters in each network layer:')]
+    for layer_index in range(1, len(LAYER_SIZES)):
+      layer_sliders = [slider for key, slider in self.sliders.items() if int(key[1]) == layer_index]
+      layers.append(ipywidgets.HBox([ipywidgets.Label(f'Layer {layer_index}:')] + layer_sliders))
+    buttons = ipywidgets.HBox(
+        [self.center_button, self.reset_button, self.randomize_button, self.fit_button]
+    )
+    rows = [selectors, *layers, buttons, self.output, self.textarea]
+    IPython.display.display(ipywidgets.VBox(rows))
 
 
 # %%
 display_network_graph()
-display_main_ui()
+main_plot = MainPlot()
+main_plot.display()
 
 # %% [markdown]
 # # End
